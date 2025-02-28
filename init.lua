988c1bd06fd30b9a3cdc963d36ade1be508436f3
kais3l <134835996+kais3l@users.noreply.github.com>
2025-02-28 21:52:54 +0530

    Add the lua-language-server and treesitter plugins

--- init.lua
+++ init.lua
@@ -1,10 +1,17 @@
 vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus"
 
 
 require("config.lazy")
 vim.keymap.set("n","<space><space>x","<cmd>source %<CR>")
 vim.keymap.set("n","<space>x",":.lua<CR>")
 vim.keymap.set("v","<space>x",":.lua<CR>")
+
+-- Ctrl+] to go to the declaration of a particular variable
+-- Ctrl+t to comeback to the previous position of the variable where you used Ctrl+]
+-- grr to see all the references of that particular variable
+-- grn to rename the variable
+-- K(capital k) shows a hover window explaining(?) the variable(?)
 
 vim.api.nvim_create_autocmd('TextYankPost',{
 	desc = 'Highlight when yanking(copying) text',
